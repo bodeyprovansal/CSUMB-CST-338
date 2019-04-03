8249fcb21c11a28110f94b8269b5024d68302eb3
TaylerMauk <44530136+TaylerMauk@users.noreply.github.com>
2019-04-02 17:31:59 -0700

    Add files via upload

--- M4/src/BarcodeImage.java
+++ M4/src/BarcodeImage.java
@@ -2,6 +2,7 @@
  * BarcodeImage.java
  * 
  * Tayler Mauk
+ * Bodey Provansal
  * 
  * Defines the pseudo data matrix structure that will represent the optical
  * bar codes.
@@ -31,14 +32,53 @@ public class BarcodeImage implements Cloneable
    
    // Read string and convert to corresponding bar code sequences
    public BarcodeImage(String[] strData)
-   {
-      
+   { 
+      if(checkSize(strData))
+      {   
+         imageData = new boolean[MAX_WIDTH][MAX_HEIGHT];       
+         char[][] strChar = new char[MAX_WIDTH][MAX_HEIGHT];
+         for (int i = 0; i < strData.length; i++)
+         {
+            strChar[i] = strData[i].toCharArray();
+         }
+         int charX = 0;
+         int charY = 0;
+         
+         for (int y = MAX_HEIGHT - 1; y >= 0; y--)
+         {
+            charX = 0;
+            charY = 0;
+            //System.out.println("y: " + y);
+            for (int x = 0; x < MAX_WIDTH - 1; x++)
+            {
+               //System.out.println("x: " + x);
+               if (strChar[charX][charY] == ' ')
+               {   
+                  this.setPixel(x, y, false);
+               }
+               if (strChar[charX][charY] == '*')
+               {   
+                  this.setPixel(x, y, true);
+               }
+               charX++;
+            }
+            charY++;
+         }
+      } 
+      else
+      {
+         System.out.println("Barcode Image must be under 30X65");
+      }
    }
    
    // Accessors
    
    public boolean getPixel(int row, int col)
    {
+      if((row < MAX_WIDTH) & (col < MAX_HEIGHT))
+      {
+         return imageData[row][col];
+      }
       return false;
    }
    
@@ -46,18 +86,68 @@ public class BarcodeImage implements Cloneable
    
    public boolean setPixel(int row, int col, boolean value)
    {
+      if((row < MAX_WIDTH) & (col < MAX_HEIGHT))
+      {
+         imageData[row][col] = value;
+         return true;
+      }
       return false;
    }
    
    @Override
-   public BarcodeImage clone()
+   public BarcodeImage clone() throws CloneNotSupportedException
    {
-      return this;
+      BarcodeImage cloneObject = new BarcodeImage();
+      
+      // Duplicate filed data
+      for (int i = 0; i < MAX_WIDTH; ++i)
+      {
+         for (int j = 0; j < MAX_HEIGHT; ++j)
+            cloneObject.imageData[i][j] = this.imageData[i][j];
+      }
+      
+      return cloneObject;
+   }
+   
+   public String displayToConsole(){
+      String displayData = "";
+      for (int i = 0; i < MAX_HEIGHT; ++i)
+      {
+         //System.out.print(" i: " + i + " ");
+         displayData += " i: " + i + " ";
+         for (int j = 0; j < MAX_WIDTH; ++j)
+         {
+            //System.out.print(t1.imageData[i][j]);
+            displayData += " j: " + j + " ";
+            displayData += imageData[i][j];            
+         }
+         displayData += "/n";        
+      }
+      return displayData;
    }
    
    // Makes sure data is not bigger than max
    private boolean checkSize(String[] data)
    {
+      if (data.length < MAX_HEIGHT)
+      {         
+         int widthCount = 0;
+         char[][] dataChar = new char[data.length][];
+         for (int i = 0; i < data.length; ++i)
+         {
+            widthCount = 0;
+            dataChar[i] = data[i].toCharArray();
+            for (int j = 0; j < dataChar[i].length; ++j)
+            {
+               widthCount++;
+               if (widthCount >= MAX_WIDTH)
+               {
+                  return false;
+               }              
+            }           
+         }
+         return true;
+      }
       return false;
    }
 }
--- M4/src/DataMatrix.java
+++ M4/src/DataMatrix.java
@@ -1,107 +1,331 @@
 /*
  * DataMatrix.java
  * 
- * [Authors]
+ * Jonathan Delgado
  * 
  * Implementation of the pseudo data matrix and its operations.
  */
 
-public class DataMatrix implements BarcodeIO
+class DataMatrix implements BarcodeIO
 {
-   // Characters to display for bar code images
    public static final char BLACK_CHAR = '*';
-   public static final char WHITE_CHAR = ''; // FIXME: Can this be a space instead?
-   
-   // The optical bar code
+   public static final char WHITE_CHAR = ' ';
    private BarcodeImage image;
-   
-   // Text to be encoded or decoded
    private String text;
-   
-   // Dimensions of the image field data
-   private int actualWidth, actualHeight;
-   
-   // Constructors
-   
+   private int actualWidth;
+   private int actualHeight;
+
+   /**
+    * Default constuctor, blank initial image and text.
+    */
    public DataMatrix()
    {
-      
+      this.image = new BarcodeImage();
+      this.text = "";
+      this.actualHeight = 0;
+      this.actualWidth = 0;
    }
-   
+
+   /**
+    * Constructor for an image.
+    */
    public DataMatrix(BarcodeImage image)
    {
-      
+      this();
+      this.scan(image);
    }
-   
+
+   /**
+    * Constructor for text.
+    */
    public DataMatrix(String text)
    {
-      
+      this();
+      this.readText(text);
    }
-   
-   // Accessors
-   
-   public int getSignalWidth()
+
+   /**
+    * Sets the text and width.
+    */
+   public boolean readText(String text)
+   {
+      if (text.length() > this.image.MAX_WIDTH)
+      {
+         return false;
+      }
+
+      this.text = text;
+      this.actualWidth = text.length();
+      return true;
+   }
+
+   /**
+    * Sets the text and actual width/height.
+    */
+   public boolean scan(BarcodeImage image)
+   {
+      try
+      {
+         this.image = image.clone();
+      } catch (CloneNotSupportedException exception)
+      {
+         return false;
+      }
+
+      this.cleanImage();
+      this.actualWidth = this.computeSignalWidth();
+      this.actualHeight = this.computeSignalHeight();
+      return true;
+   }
+
+   /**
+    * Gets the actual width.
+    */
+   public int getActualWidth()
    {
       return actualWidth;
    }
-   
-   public int getSignalHeight()
+
+   /**
+    * Gets the actual height.
+    */
+   public int getActualHeight()
    {
       return actualHeight;
    }
-   
-   // Uses bottom BLACK row of bar code to determine actualWidth
+
+   /**
+    * Determines the max width of the image.
+    */
    private int computeSignalWidth()
    {
-      return 0;
+      int width = 0;
+      while (this.image.getPixel(width, 0))
+      {
+         width++;
+      }
+      return width;
    }
-   
-   // Uses left column of bar code to determine actualHeight
+
+   /**
+    * Determines the max height of the image.
+    */
    private int computeSignalHeight()
    {
-      return 0;
-   }
-   
-   // Aligns signal to lower-left corner of 2D array
-   private void cleanImage()
-   {
-      
-   }
-   
-   @Override
-   public boolean scan(BarcodeImage bc)
-   {
-      return false;
+      int height = 0;
+      while (this.image.getPixel(0, height))
+      {
+         height++;
+      }
+      return height;
    }
 
-   @Override
-   public boolean readText(String text)
-   {
-      return false;
-   }
-
-   @Override
+   /**
+    * Generates an image from the set text.
+    */
    public boolean generateImageFromText()
    {
-      return false;
+      this.image = new BarcodeImage();
+
+      // Initial sweep to generate image
+      int textLength = this.text.length();
+      // Adds two for the border
+      this.actualWidth = textLength + 2;
+      // Eight rows plus 2 border
+      this.actualHeight = 10;
+
+      for (int i = 0; i < textLength; i++)
+      {
+         int castedCharacter = (int)this.text.charAt(i - 1);
+         this.writeCharToCol(i, castedCharacter);
+      }
+
+      // Second sweep for top/bottom borders
+      for (int x = 0; x < this.actualWidth; x++)
+      {
+         this.image.setPixel(this.actualHeight - 1, x, true);
+         if (x % 2 == 0)
+         {
+            this.image.setPixel(0, x, true);
+         }
+      }
+
+      // Third sweep for left/right borders
+      for (int y = 0; y < this.actualHeight; y++)
+      {
+         image.setPixel(this.actualHeight - 1 - y, 0, true);
+         if (y % 2 == 0)
+         {
+            image.setPixel(this.actualHeight - 1 - y, this.actualWidth - 1, true);
+         }
+      }
+
+      return true;
    }
 
-   @Override
+   /**
+    * Converts set image into text.
+    */
    public boolean translateImageToText()
    {
-      return false;
+      this.text = "";
+      // Padding on iterators for borders
+      for (int x = 1; x < this.actualWidth - 1; x++)
+      {
+         this.text += readCharFromCol(x);
+      }
+      return true;
    }
 
-   @Override
+   /**
+    * Logs text to the console.
+    */
    public void displayTextToConsole()
    {
-      
+      System.out.println(this.text);
    }
 
-   @Override
+   /**
+    * Prints the image to the console.
+    */
    public void displayImageToConsole()
    {
-      
+      // Top border
+      for (int x = 0; x < this.actualWidth + 2; x++)
+      {
+         System.out.print("-");
+      }
+      System.out.println();
+
+      // Data
+      for (int y = 0; y < this.image.MAX_HEIGHT; y++)
+      {
+         System.out.print("|");
+         for (int x = 0; x < this.actualWidth; x++)
+         {
+            if (this.image.getPixel(x, y))
+            {
+               System.out.print(this.BLACK_CHAR);
+            } else {
+               System.out.print(this.WHITE_CHAR);
+            }
+         }
+         System.out.println("|");
+         System.out.println();
+      }
+
+      // Bottom border
+      for (int x = 0; x < this.actualWidth + 2; x++)
+      {
+         System.out.print("-");
+      }
+      System.out.println();
    }
 
+   /**
+    * Normalizes the image and moves it to the lower left.
+    */
+   private void cleanImage()
+   {
+      this.moveImageToLowerLeft();
+   }
+
+   /**
+    * Resets an image and moves it to the lower left.
+    */
+   private void moveImageToLowerLeft()
+   {
+      int bottom = 0;
+      for (int x = 0; x < this.image.MAX_WIDTH; x++)
+      {
+         // Determine if there is anything on that line
+         for (int y = 0; y < this.image.MAX_HEIGHT; y++)
+         {
+            if (this.image.getPixel(x, y))
+            {
+               bottom = y;
+               break;
+            }
+         }
+      }
+
+      int left = 0;
+      for (int x = 0; x < this.image.MAX_WIDTH; x++)
+      {
+         if (this.image.getPixel(x, bottom))
+         {
+            left = x;
+            break;
+         }
+      }
+
+      shiftImageDown(bottom);
+      shiftImageLeft(left);
+   }
+
+   /**
+    * Shifts the entire image down `offset`.
+    */
+   private void shiftImageDown(int offset)
+   {
+      for (int y = offset; y < this.image.MAX_HEIGHT; y++)
+      {
+         for (int x = 0; x < this.image.MAX_WIDTH; x++)
+         {
+            this.image.setPixel(x, (y - offset), this.image.getPixel(x, y));
+         }
+      }
+   }
+
+   /**
+    * Shits the entire image left `offset`.
+    */
+   private void shiftImageLeft(int offset)
+   {
+      for (int x = offset; x < image.MAX_WIDTH; x++)
+      {
+         for (int y = 0; y < image.MAX_HEIGHT; y++)
+         {
+            image.setPixel((x - offset), y, this.image.getPixel(x, y));
+         }
+      }
+   }
+
+   /**
+    * Read a character from a column.
+    */
+   private char readCharFromCol(int col)
+   {
+      int total = 0;
+      for (int y = 1; y < this.actualHeight - 1; y++)
+      {
+         if (this.image.getPixel(col, y))
+         {
+            total += Math.pow(2, y - 1);
+         }
+      }
+      return (char)total;
+   }
+
+   /**
+    * Writes a character to a column.
+    */
+   private boolean writeCharToCol(int col, int code)
+   {
+      int bitwise = 0;
+      for (int y = 0; y < image.MAX_HEIGHT; y++)
+      {
+         bitwise = (int)Math.pow(2, y);
+         if (bitwise <= code)
+         {
+            // https://stackoverflow.com/questions/17256644/how-does-the-bitwise-and-work-in-java
+            if ((code & bitwise) == bitwise)
+            {
+               this.image.setPixel(col, y + 1, true);
+            }
+         }
+      }
+
+      return true;
+   }
 }
